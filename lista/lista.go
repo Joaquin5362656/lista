294001f2078714c3990068143fad3db0b52823e1
Sebas <sebastiancormano9@gmail.com>
2024-04-26 17:55:46 -0300

    Implementacion de iterador externo sin probar

--- lista/lista.go
+++ lista/lista.go
@@ -9,13 +9,39 @@ type Lista[T any] interface {
 	VerUltimo() T
 	Largo() int
 	Iterar(visitar func(T) bool)
+
+	// Iterador crea el iterador que permite recorrer los elementos de la lista, usando
+	// las primitivas de este
 	Iterador() IteradorLista[T]
 }
 
 type IteradorLista[T any] interface {
+
+	// VerActual devuelve el elemento en la instancia de la iteracion en que se encuentra el iterador.
+	// Si se iteraron todos los elementos entra en panico con el mensaje "El iterador termino de iterar"
 	VerActual() T
+
+	// HaySiguiente devuelve true si quedan elementos en la lista para iterar y devuelve false si ya
+	// se iteraron todos los elementos y no queda ningun otro por iterar.
 	HaySiguiente() bool
+
+	// Siguiente hace que avanze a otra instancia del iterador. Haciendo que pase a otro elemento
+	// de la lista listo para ser iterado.
 	Siguiente()
+
+	// Insertar agrega un nuevo elemento en la lista en la posicion actual en la que se encuentra el
+	// iterador, pasando a una instancia donde este nuevo elemento sea el nuevo actual y este listo
+	// para ser iterado y el elemento que anteriormente estaba en la posicion actual pasa a ser
+	// el siguiente elemento a iterar.
+	// Al insertar se aumenta el largo de la lista y en caso de insertarse al principio o al final
+	// de la lista modifica el primer elemento o el ultimo de la lista
 	Insertar(T)
+
+	// Borrar elimina el elemento en la posicion actual en la que se encuentra el iterador,
+	// disminuyendo el largo de la lista y pasando a la siguiente instancia de la iteracion
+	// con el siguiente elemento de la lista.
+	// En caso de borrar el primer elemento de la lista, el primero pasa a ser el siguiente
+	// elemento de la lista y en caso de ser el elemento al final de la lista, el ultimo
+	// pasa a ser el anterior elemento a este
 	Borrar() T
 }
--- lista/lista_enlazada.go
+++ lista/lista_enlazada.go
@@ -5,6 +5,10 @@ type nodoLista[T any] struct {
 	siguiente *nodoLista[T]
 }
 
+func crearNodoLista[T any](dato T, siguiente *nodoLista[T]) *nodoLista[T] {
+	return &nodoLista[T]{dato, siguiente}
+}
+
 type listaEnlazada[T any] struct {
 	primero *nodoLista[T]
 	ultimo  *nodoLista[T]
@@ -12,9 +16,95 @@ type listaEnlazada[T any] struct {
 }
 
 type iteradorLista[T any] struct {
-	//
+	lista    *listaEnlazada[T]
+	actual   *nodoLista[T]
+	anterior *nodoLista[T]
 }
 
 func CrearListaEnlazada[T any]() Lista[T] {
 	return &listaEnlazada[T]{}
+}
+
+func (lista *listaEnlazada[T]) Iterador() iteradorLista[T] {
+	return iteradorLista[T]{lista, lista.primero, nil}
+}
+
+func (iterador *iteradorLista[T]) VerActual() T {
+
+	if iterador.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+
+	return iterador.actual.dato
+}
+
+func (iterador *iteradorLista[T]) HaySiguiente() bool {
+
+	if iterador.actual == nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (iterador *iteradorLista[T]) Siguiente() {
+
+	if iterador.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+
+	anteriorLista := iterador.actual
+	iterador.actual = iterador.actual.siguiente
+	iterador.anterior = anteriorLista
+}
+
+func (iterador *iteradorLista[T]) Insertar(dato T) {
+
+	anteriorLista := iterador.anterior
+	esFinalLista := iterador.actual == nil
+	esInicioLista := anteriorLista == nil
+
+	iterador.actual = crearNodoLista(dato, iterador.actual)
+
+	if esInicioLista {
+		iterador.lista.primero = iterador.actual
+	} else {
+		anteriorLista.siguiente = iterador.actual
+	}
+
+	if esFinalLista {
+		iterador.lista.ultimo = iterador.actual
+	}
+
+	iterador.lista.largo++
+}
+
+func (iterador *iteradorLista[T]) Borrar() T {
+
+	if iterador.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+
+	elementoABorrar := iterador.actual.dato
+
+	esFinalLista := iterador.actual == nil
+	esInicioLista := iterador.anterior == nil
+
+	iterador.actual = iterador.actual.siguiente
+
+	if esInicioLista {
+		iterador.lista.primero = iterador.actual
+	} else {
+		anteriorLista := iterador.anterior
+		anteriorLista.siguiente = iterador.actual
+	}
+
+	if esFinalLista {
+		iterador.lista.ultimo = iterador.anterior
+	}
+
+	iterador.lista.largo--
+
+	return elementoABorrar
+
 }
