8b7b1e091f6a70f932a13c9871ffba2d31597a72
Sebas <sebastiancormano9@gmail.com>
2024-04-28 10:43:27 -0300

    Implementacion de todas las prueba iterador interno

--- lista/lista_test.go
+++ lista/lista_test.go
@@ -65,6 +65,82 @@ func TestIteradorInternoTodosLosElementos(t *testing.T) {
 	require.Equalf(t, variosString, borrarPrimerosNElementos(listaVariosString, listaVariosString.Largo()), "Se puede iterar una lista de strings, aplicando la funcion a cada elemento en el orden y forma correcta")
 }
 
+func TestIteradorInternoHastaDevolverFalse(t *testing.T) {
+
+	var (
+		unNumeroCumpleCond            = []*int{proElemento(5)}
+		unNumeroNoCumple              = []*int{proElemento(2)}
+		numerosImparesMenosUno        = []*int{proElemento(3), proElemento(7), proElemento(13), proElemento(4), proElemento(11), proElemento(1)}
+		positivosInicioNegativosFinal = []*int{proElemento(3), proElemento(6), proElemento(7), proElemento(-3), proElemento(-8), proElemento(-31)}
+		primerNumeroNoCumple          = []*int{proElemento(6), proElemento(5), proElemento(2), proElemento(7), proElemento(8)}
+	)
+	var (
+		listaUnNumeroCumpleCond            = TDALista.CrearListaEnlazada[*int]()
+		listaUnNumeroNoCumple              = TDALista.CrearListaEnlazada[*int]()
+		listaNumerosImparesMenosUno        = TDALista.CrearListaEnlazada[*int]()
+		listaPositivosInicioNegativosFinal = TDALista.CrearListaEnlazada[*int]()
+		listaPrimerNumeroNoCumple          = TDALista.CrearListaEnlazada[*int]()
+	)
+	var (
+		multiplicarx2TodosLosElementos = func(numero *int) bool {
+			*numero = *numero * 2
+			return true
+		}
+		multiplicarx2HastaEncontrarPar = func(numero *int) bool {
+			if *numero%2 == 1 {
+				*numero = *numero * 2
+				return true
+			} else {
+				return false
+			}
+		}
+		invertirSignoATodos = func(numero *int) bool {
+			*numero = *numero * -1
+			return true
+		}
+		pasarAElementosNegativosHastaEncontraUno = func(numero *int) bool {
+
+			if *numero <= 0 {
+				return false
+			}
+
+			*numero = *numero * -1
+			return true
+		}
+	)
+
+	insertarArrayALista(unNumeroCumpleCond, listaUnNumeroCumpleCond, false)
+	listaUnNumeroCumpleCond.Iterar(multiplicarx2HastaEncontrarPar)
+	visitarArray(unNumeroCumpleCond, multiplicarx2TodosLosElementos)
+	require.Equalf(t, unNumeroCumpleCond, borrarPrimerosNElementos(listaUnNumeroCumpleCond, listaUnNumeroCumpleCond.Largo()), "Se itera una lista con un solo elemento que cumple la condicion aplicandole la funcion correspondiente")
+
+	insertarArrayALista(unNumeroNoCumple, listaUnNumeroNoCumple, false)
+	listaUnNumeroNoCumple.Iterar(multiplicarx2HastaEncontrarPar)
+	visitarArray(unNumeroNoCumple, multiplicarx2HastaEncontrarPar)
+	require.Equalf(t, unNumeroNoCumple, borrarPrimerosNElementos(listaUnNumeroNoCumple, listaUnNumeroNoCumple.Largo()), "Se itera una lista con un unico elemento que devuelve false y se cumple lo indicado en la funcion correctamente")
+
+	insertarArrayALista(numerosImparesMenosUno, listaNumerosImparesMenosUno, false)
+	listaNumerosImparesMenosUno.Iterar(multiplicarx2HastaEncontrarPar)
+	visitarArray(numerosImparesMenosUno, multiplicarx2HastaEncontrarPar)
+	require.Equalf(t, numerosImparesMenosUno, borrarPrimerosNElementos(listaNumerosImparesMenosUno, listaNumerosImparesMenosUno.Largo()), "Se iteran correctamente aplicando la funcion a cada elemento hasta encontrar uno que devuelva false, dejando de iterar elementos")
+
+	insertarArrayALista(positivosInicioNegativosFinal, listaPositivosInicioNegativosFinal, false)
+	listaPositivosInicioNegativosFinal.Iterar(pasarAElementosNegativosHastaEncontraUno)
+	visitarArray(positivosInicioNegativosFinal, invertirSignoATodos)
+	require.NotEqualf(t, positivosInicioNegativosFinal, borrarPrimerosNElementos(listaPositivosInicioNegativosFinal, listaPositivosInicioNegativosFinal.Largo()), "Al iterar una lista donde un elemento devuelve false, no se iteran todos los elementos y no se le aplica la funcion a todos estos")
+
+	visitarArray(positivosInicioNegativosFinal, invertirSignoATodos)
+
+	insertarArrayALista(positivosInicioNegativosFinal, listaPositivosInicioNegativosFinal, false)
+	listaPositivosInicioNegativosFinal.Iterar(pasarAElementosNegativosHastaEncontraUno)
+	visitarArray(positivosInicioNegativosFinal, pasarAElementosNegativosHastaEncontraUno)
+	require.Equalf(t, positivosInicioNegativosFinal, borrarPrimerosNElementos(listaPositivosInicioNegativosFinal, listaPositivosInicioNegativosFinal.Largo()), "Al iterar una lista donde un elementos devuelve false, se iteran correctamente los primeros elementos hasta encontrar el que devuelve false")
+
+	insertarArrayALista(primerNumeroNoCumple, listaPrimerNumeroNoCumple, false)
+	listaPrimerNumeroNoCumple.Iterar(multiplicarx2HastaEncontrarPar)
+	require.Equal(t, primerNumeroNoCumple, borrarPrimerosNElementos(listaPrimerNumeroNoCumple, listaPrimerNumeroNoCumple.Largo()), "En una lista que se itera, si el primer elemento devuelve false, deja de iterar no aplicando la funcion a ningun elemento y dejando la lista igual que al inicio")
+}
+
 func proElemento[T any](dato T) *T {
 
 	nuevoProT := new(T)
