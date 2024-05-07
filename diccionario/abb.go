320b149adc67610363b2a582fda3fb3f0cac715e
Joaquin Osorio <osoriojoaquin785@gmail.com>
2024-05-07 13:16:54 -0300

    archivos para tda abb

---
+++ diccionario/abb.go
@@ -0,0 +1,18 @@
+package diccionario
+
+type nodoAbb[K comparable, V any] struct {
+	izquierdo *nodoAbb[K, V]
+	derecho   *nodoAbb[K, V]
+	clave     K
+	dato      V
+}
+
+type abb[K comparable, V any] struct {
+	raiz     *nodoAbb[K, V]
+	cantidad int
+	cmp      funcCmp[K]
+}
+
+func CrearABB[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
+
+}
